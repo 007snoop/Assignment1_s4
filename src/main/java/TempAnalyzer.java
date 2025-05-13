87efd0045a81637fe6116d672e339a41dcc47715
Colin Yetman <colinguitarist@gmail.com>
2025-05-13 15:33:37 -0230

    memory update

--- src/main/java/TempAnalyzer.java
+++ src/main/java/TempAnalyzer.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class TempAnalyzer {
     public static void main(String[] args) {
+        int sum = 0;
+        int countAboveAverage = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter The Number of days: ");
         int numDays = scanner.nextInt();
-
         int[] temps = new int[numDays];
-        int sum = 0;
+
 
         for (int i = 0; i < numDays; i++) {
             System.out.printf("Enter temp for day " + (i + 1) + ": ");
@@ -18,15 +19,14 @@ public class TempAnalyzer {
         }
 
         double average = (double) sum / numDays;
-        System.out.printf("\nAverage temperature: " + average);
 
-        int countAboveAverage = 0;
         for (int temp : temps) {
             if (temp > average) {
                 countAboveAverage++;
             }
         }
 
+        System.out.printf("\nAverage temperature: " + average);
         System.out.printf("\nNumber of days above average temperature: " + countAboveAverage);
     }
 }
